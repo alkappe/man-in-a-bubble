98f3a4ed7c5fdbfe89f2ae0edf14fdf5e664513f
alkappe <alka.cappellazzo@gmail.com>
2019-04-07 23:31:36 +0200

    added human

---
+++ data.js
@@ -0,0 +1,82 @@
+window.data = [{
+  nome: "bubble1", // { r: }
+  radius: 10,
+  url: "figure/1om.png"
+},
+{
+  nome: "bubble2",
+  radius: 40,
+  url: "figure/2om.png"
+},
+{
+  nome: "bubble3",
+  radius: 8,
+  url: "figure/3om.png"
+},
+{
+  nome: "bubble4",
+  radius: 10,
+  url: "figure/4om.png"
+},
+{
+  nome: "bubble5",
+  radius: 5,
+  url: "figure/5om.png"
+},
+{
+  nome: "bubble6",
+  radius: 70,
+  url: "figure/6om.png"
+},
+{
+  nome: "bubble7",
+  radius: 100,
+  url: "figure/7om.png"
+},
+{
+  nome: "bubble10",
+  radius: 20,
+  url: "figure/8om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/6om.png"
+},
+{
+  nome: "bubble8",
+  radius: 50,
+  url: "figure/3om.png"
+},
+{
+  nome: "bubble9",
+  radius: 60,
+  url: "figure/7om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/7om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/1om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/5om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/3om.png"
+},
+{
+  nome: "bubble10",
+  radius: 30,
+  url: "figure/8om.png"
+}
+
+]
